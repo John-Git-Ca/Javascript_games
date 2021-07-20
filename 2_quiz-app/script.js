1e37bbcb625de2353d5eb5956a5cecf33ccfcc62
John Li <john.lizuhua@outlook.com>
2021-07-20 02:44:39 -0400

    second project

---
+++ 2_quiz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'how old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: c
+    },
+    {
+        question: 'what is the best programming language in 2021?',
+        a:'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    },
+    {
+        question: 'who is the president of US',
+        a: 'Forin',
+        b: 'Donald Trump',
+        c: 'Ivan',
+        d: 'Obama',
+        correct: 'b'
+    },
+    {
+        question: 'what does HTML stand for?',
+        a:'Hypertext Markup Language',
+        b: 'Cacading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programming Interface',
+        correct: 'a'
+    },
+    {
+        question: 'what year was JavaScript launched?',
+        a: '2020',
+        a: '2021',
+        a: '2022',
+        a: '1994',
+        correct:'d'
+    }
+]
