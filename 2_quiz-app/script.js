e0238c4bfe46b8166119b1a5702db15f4e05e56e
John Li <john.lizuhua@outlook.com>
2021-07-21 00:57:03 -0400

    third app

--- 2_quiz-app/script.js
+++ 2_quiz-app/script.js
@@ -34,9 +34,56 @@ const quizData = [
     {
         question: 'what year was JavaScript launched?',
         a: '2020',
-        a: '2021',
-        a: '2022',
-        a: '1994',
+        b: '2021',
+        c: '2022',
+        d: '1994',
         correct:'d'
     }
 ]
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+var currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerHTML = currentQuizData.question;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+}
+
+function getSelected(){
+    const answersEls = document.querySelectorAll(".answer");
+    answersEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            return answerEl.id;
+        }
+
+        return undefined;
+    })
+}
+
+submitBtn.addEventListener("click", () =>{
+    currentQuiz++;
+    const answer = getSelected();
+
+    if(answer ){
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }
+        else{
+            //TODO: Show results
+            alert("you finished");
+        }
+    }
+})
